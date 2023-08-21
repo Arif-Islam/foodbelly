a0f948b6b3d14e7eca11181d851629edaf395ff5
Arif <iariful950@gmail.com>
2023-08-22 00:48:29 +0600

    [DONE] menupage design

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,7 +1,11 @@
+import { pizzas } from "@/data";
+import Link from "next/link";
 import React from "react";
 
 const Category = () => {
-  return <div>Category</div>;
+  return <div className="flex text-red-500">
+    {pizzas.map((item) => <Link href={`/product/${item.id}`} className="" key={item.id}></Link>)}
+  </div>;
 };
 
 export default Category;
